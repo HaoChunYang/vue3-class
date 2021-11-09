2606a5c84664f95c9b2a95a9c5e367f691a2d9d4
HaoChunYang <lanianer@qq.com>
2021-11-09 18:42:18 +0800

    vuex相关代码

--- todo-list/src/components/Count.vue
+++ todo-list/src/components/Count.vue
@@ -1,18 +1,24 @@
 <template>
   <div @click="add">count: {{count}}</div>
+  <p>{{`${count} * 2 = ${double}`}}</p>
+  <button @click="asyncAdd">async add</button>
 </template>
 
 <script setup>
 import { computed } from '@vue/reactivity'
-// import { useStore } from 'vuex'
-import { useStore } from '../store/hvuex.js'
+import { useStore } from 'vuex'
+// import { useStore } from '../store/hvuex.js'
 let store = useStore()
 console.log('count vue :', store.state)
 let count = computed(() => store.state.count)
+let double = computed(() => store.getters.double)
 
 function add () {
   store.commit('add')
   console.log('count vue add :', store.state)
+}
+function asyncAdd () {
+  store.dispatch('asyncAdd')
 }
 </script>
 
--- todo-list/src/store/index.js
+++ todo-list/src/store/index.js
@@ -1,5 +1,5 @@
-// import { createStore } from "vuex";
-import { createStore } from "./hvuex.js"
+import { createStore } from "vuex";
+// import { createStore } from "./hvuex.js"
 
 const store = createStore({
   state () {
@@ -7,10 +7,22 @@ const store = createStore({
       count: 666
     }
   },
+  getters: {
+    double (state) {
+      return state.count * 2
+    }
+  },
   mutations: {
     add (state) {
       console.log('mutaions-add:', state)
       state.count++
+    }
+  },
+  actions: {
+    asyncAdd ({commit}) {
+      setTimeout(() => {
+        commit('add')
+      }, 1000)
     }
   }
 })
